c55b1ac9619490436795841a1de7edc78bef39a7
HIghJ <822179429@qq.com>
2022-07-20 19:38:01 +0800

    HighJ

---
+++ src/main/java/com/example/shakespeare/ShakespeareApplication.java
@@ -0,0 +1,13 @@
+package com.example.shakespeare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakespeareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShakespeareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shakespeare/ShakespeareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shakespeare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakespeareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
